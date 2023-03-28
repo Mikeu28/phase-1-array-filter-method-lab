b99a9c83165da4786a43e7b5a9d19a3cf4006dc7
Mike Urezzio <mikeurezzio@gmail.com>
2023-03-28 19:41:02 -0400

    array filter lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching (arrayData, name) {
+    return arrayData.filter (firstName => firstName.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch (arrayData, name) {
+    return arrayData.filter (function (driver) {
+        return driver.toLowerCase().substring(0, name.length) === name.toLowerCase()
+    })
+}
+
+function matchName (arrayData, firstName) {
+    return arrayData.filter(driver => driver.name === firstName)
+}
